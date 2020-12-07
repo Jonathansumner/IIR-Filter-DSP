c499b441e5dea99fea682eae7b7726afda582b6f
Jonathan Sumner <jonathansumner98@gmail.com>
2020-12-07 12:47:09 +0000

    IIR Implementation

--- IIRFilter.py
+++ IIRFilter.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class IIR2Filter:
     def __init__(self, coeff):
         self.coeff = coeff
@@ -16,5 +19,29 @@ class IIR2Filter:
 
         self.accumulator_in = self.input + self.buffer1 * -self.IIRcoeff[1] + self.buffer2 * -self.IIRcoeff[2]
 
+        self.buffer2 = self.buffer1
+        self.buffer1 = self.accumulator_in
+        self.input = self.accumulator_out
+        self.output = self.accumulator_out
+        return self.output
+
     class IIRFilter:
-        pass
+        def __init__(self, coeff):
+            self.coeff = coeff
+            self.accumulator_input = np.zero(len(self.coeff))
+            self.accumulator_output = np.zero(len(self.coeff))
+            self.buffer1 = np.zeros(len(self.coeff))
+            self.buffer2 = np.zeros(len(self.coeff))
+            self.input = 0
+            self.output = 0
+            self.IIRList = []
+
+            for i in range(len(self.coeff)):
+                self.IIRList.append(IIR2Filter(self.coeff[i]))
+
+        def filter(self, input):
+            self.input = input
+            self.output = 0
+            for i in range(0, len(self.coeff)):
+                self.output = self.IIRList[i].filter(self.output)
+            return self.output
